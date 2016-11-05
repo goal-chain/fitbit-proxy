31338680f5b8b2312a536c0f180468110d162dc0
Conor Fennell <conor@Conors-MacBook-Pro.local>
2016-11-05 23:53:23 +0000

    send back json

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/getsteps/:token', function(req, res) {
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + req.params.token;
           axios.get('https://api.fitbit.com/1/user/-/activities/date/2016-11-05.json', {})
                   .then(function (response) {
-                    res.send({steps:response.data.summary.steps});
+                    res.json({steps:response.data.summary.steps});
                   })
                   .catch(function (error) {
                     res.send({error: 'epic'})
