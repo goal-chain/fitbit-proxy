98ba9228110d26a6c5ccd016c1f14f50f20828c5
Conor Fennell <conor@Conors-MacBook-Pro.local>
2016-11-05 23:41:09 +0000

    added path for fitbit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var axios = require('axios');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +12,17 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/getsteps/:token', function(req, res) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + req.params.token;
+          axios.get('https://api.fitbit.com/1/user/-/activities/date/2016-11-05.json', {})
+                  .then(function (response) {
+                    res.send({steps:response.data.summary.steps});
+                  })
+                  .catch(function (error) {
+                    res.send({error: 'epic'})
+                  });
 });
 
 app.listen(app.get('port'), function() {
